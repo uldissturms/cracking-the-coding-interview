7ae11b65e4a214596236e222c84bfe263ac7c5be
Uldis Sturms <uldis.sturms@gmail.com>
2017-06-26 22:08:03 +0100

    run all chapter tests

--- run.sh
+++ run.sh
@@ -1,1 +1,5 @@
-(cd chapter_1; runhaskell UniqueTests.hs);
+for chapter in `find . -name chapter_* -type d`
+do
+  echo "running chapter: $chapter tests..."
+  (cd $chapter; for test in `find . -name *Tests.hs`; do runhaskell $test; done);
+done
